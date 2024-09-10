b7fe3eef7374548d217c26542037cc08df104d07
Sidd2712 <siddharth.2727goyal@gmail.com>
2024-09-11 00:10:30 +0530

    ver

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -89,12 +89,13 @@ const Contact = () => {
       service: value
     });
   };
+  axios.defaults.withCredentials=true;
 
   const handleSubmit = async (e) => {
     e?.preventDefault();
     console.log('form data: ', formData)
     try {
-      const response = await axios.post('http://localhost:5000/submit', formData);
+      const response = await axios.post('https://sidportfolio-api.vercel.app/submit', formData);
       alert('Form submitted successfully');
       setFormData(initialFormData);
     } catch (error) {

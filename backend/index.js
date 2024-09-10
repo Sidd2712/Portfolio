de1e51b446d972a694e5c042061137ebe7f816c5
Siddharth <114308165+Sidd2712@users.noreply.github.com>
2024-09-11 00:01:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,9 @@ app.use(cors(
   }
 ));
 app.use(express.json());
+app.get("/",(req,res)=>{
+  res.json("Hello");
+})
 
 // Connect to MongoDB
 // mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
